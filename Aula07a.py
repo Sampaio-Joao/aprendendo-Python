7caa1b3aaae0aae2c4344fe7c9457933032170e7
João Pedro Sampaio de Almeida <jpsam2011@gmail.com>
2026-02-12 19:48:59 -0300

    Aula07 Gustavo Guanabara

---
+++ Aula07a.py
@@ -0,0 +1,56 @@
+#nome = input('Qual o seu nome?')
+#print('Prazer em te conhecer {:-^20}!'.format(nome)) | ^CENTRALIZA >DEIXA A DIREITA < DEIXA A ESQUERDA
+
+#n1 = int(input('Digite um valor'))
+#n2 = int(input('Digite outro valor'))
+#s = n1 + n2
+#sub = n1 - n2
+#m = n1 * n2
+#d = n1 / n2
+#di = n1 // n2
+#rd = n1 % n2
+#e = n1 ** n2
+#print('A soma é {0}, a subtração é {1}\nA multiplicação é {2}, a divisão é {3:.2f}\nA divisão inteira é {4}, o resto da divisão é {5} e a exponenciação é {6}'.format(s,sub,m,d,di,rd,e))
+
+
+##Desafio Gustavo Guanabara: Antecessor e sucessor
+
+#num = int(input('Digite um número: '))
+#antecessor = num - 1
+#sucessor = num + 1
+#print("O antecessor desse número é {}, e o sucessor é {}.".format(antecessor, sucessor))
+
+
+##Desafio Gustavo Guanabara: Mostrar o dobro, o triplo e a raiz quadrada
+
+#num = int(input('Digite um número para saber o dobro, o trilo e a raiz quadrada: '))
+#dobro = num * 2
+#triplo = num * 3
+#raiz = num ** (1/2)
+#print("O dobro do seu número é {}, o triplo é {} e a raiz quadrada é {}".format(dobro, triplo, raiz))
+
+
+##Desafio Gustavo Guanabara: Calcular a média da nota de um aluno
+
+#aluno = input('Olá! Qual o seu nome?')
+#num1 = int(input(f'Bem vindo {aluno}! Qual a sua primeira nota? '))
+#num2 = int(input('E qual a segunda nota? '))
+#num3 = int(input('Agora sua terceira nota '))
+#media = (num1 + num2 + num3) / 2
+#print('Sua média foi {:.2f}'.format(media))
+
+
+##Desafio Gustavo Guanabara: Transformar metro em cm e mm
+
+#metros = float(input('Digite quantos metros tem a ponte que você quer construir: '))
+#cm = metros * 100
+#mm = metros * 1000
+#print('Essa metragem em centimetros é {:.0f} e em milimetros é {:.0f}'.format(cm, mm))
+
+
+#Desafio Gustavo Guanabara: Mostrar a tabuada do número que a pessoa inserir
+
+num = float(input('digite um numero: '))
+tabuada = num * 1, num * 2, num * 3, num * 4, num * 5, num * 6, num * 7, num * 8, num * 9, num * 10
+print(f" A tabuada desse número é {tabuada}")
+
