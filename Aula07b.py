41da3b935d5ec492ad4229a123242f8bdc678af0
João Pedro Sampaio de Almeida <jpsam2011@gmail.com>
2026-02-14 17:27:35 -0300

    + Desafios do Gustavo Guanabara

---
+++ Aula07b.py
@@ -0,0 +1,88 @@
+#nome = input('Qual o seu nome?')
+#print('Prazer em te conhecer {:-^20}!'.format(nome)) | ^CENTRALIZA >DEIXA A DIREITA < DEIXA A ESQUERDA
+
+#n1 = int(input('Digite um valor'))
+#n2 = int(input('Digite outro valor'))
+#s = n1 + n2
+#sub = n1 - n2
+#m = n1 * n2
+#d = n1 / n2
+#di = n1 // n2
+#rd = n1 % n2
+#e = n1 ** n2
+#print('A soma é {0}, a subtração é {1}\nA multiplicação é {2}, a divisão é {3:.2f}\nA divisão inteira é {4}, o resto da divisão é {5} e a exponenciação é {6}'.format(s,sub,m,d,di,rd,e))
+
+
+##Desafio Gustavo Guanabara: Antecessor e sucessor
+
+#num = int(input('Digite um número: '))
+#antecessor = num - 1
+#sucessor = num + 1
+#print("O antecessor desse número é {}, e o sucessor é {}.".format(antecessor, sucessor))
+
+
+##Desafio Gustavo Guanabara: Mostrar o dobro, o triplo e a raiz quadrada
+
+#num = int(input('Digite um número para saber o dobro, o trilo e a raiz quadrada: '))
+#dobro = num * 2
+#triplo = num * 3
+#raiz = num ** (1/2)
+#print("O dobro do seu número é {}, o triplo é {} e a raiz quadrada é {}".format(dobro, triplo, raiz))
+
+
+##Desafio Gustavo Guanabara: Calcular a média da nota de um aluno
+
+#aluno = input('Olá! Qual o seu nome?')
+#num1 = int(input(f'Bem vindo {aluno}! Qual a sua primeira nota? '))
+#num2 = int(input('E qual a segunda nota? '))
+#num3 = int(input('Agora sua terceira nota '))
+#media = (num1 + num2 + num3) / 2
+#print('Sua média foi {:.2f}'.format(media))
+
+
+##Desafio Gustavo Guanabara: Transformar metro em cm e mm
+
+#metros = float(input('Digite quantos metros tem a ponte que você quer construir: '))
+#cm = metros * 100
+#mm = metros * 1000
+#print('Essa metragem em centimetros é {:.0f} e em milimetros é {:.0f}'.format(cm, mm))
+
+
+##Desafio Gustavo Guanabara: Mostrar a tabuada do número que a pessoa inserir
+
+num = float(input('digite um numero: ').replace(",", "."))
+t1 = num * 1; t2 = num * 2; t3 = num * 3; t4 = num * 4; t5 = num * 5; t6 = num * 6; t7 = num * 7; t8 = num * 8; t9 = num * 9; t10 = num * 10
+print(f"Tabuada do seu número do 1 ao 10:\n{num} x 1 = {t1:.2f}\n{num} x 2 = {t2:.2f}\n{num} x 3 = {t3:.2f}\n{num} x 4 = {t4:.2f}\n{num} x 5 = {t5:.2f}\n{num} x 6 = {t6:.2f}\n{num} x 7 = {t7:.2f}\n{num} x 8 = {t8:.2f}\n{num} x 9 = {t9:.2f}\n{num} x 10 = {t10:.2f}")
+
+
+
+##desafio Gustavo Guanabara: Ler quanto de dinheiro que tem na carteira e mostrar quantos Doláres pode comprar
+
+#carteira = int(input('Quanto reais você tem na sua carteira? '))
+#dolar = carteira/5.22
+#print(f'Com essa quantidade dá para você ter {dolar:.2f} Doláres.')
+
+
+##Desafio Gustavo Guanabara: Calcular altura e largura virando m**2 e quando de tinta precisa, sabendo que 1 litro de tinta cobre 2m**2
+
+#largura = int(input("Qual a largura da sua parede? Em metros "))
+#altura = int(input("Qual a altura da sua parede? Em metros "))
+#m2 = largura * altura
+#lt = m2 / 2
+#print(f'Sua parede tem {m2} metros quadrados, vai ser necessário {lt} litros de tinta para pintar sua parede')
+
+
+##Desafio Gustavo Guanabara: Algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto
+
+#valor = int(input("Valor do produto: "))
+#desconto = valor * 5 / 100
+#valor_final = valor - desconto
+#print(f'o valor final do seu produto é {valor_final} reais.')
+
+
+##Desafio Gustavo Guanabara: Ler Salário e aumente 15%
+
+#salario = int(input('Qual o seu salário? '))
+#aumento =  salario + (salario * 15 / 100)
+#print(f'Seu salário aumentou 15%, e ficou em {aumento} reais')
+
